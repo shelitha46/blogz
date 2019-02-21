46288968e1cfe3c88023dfb9b4acf14270298022
Shelitha Peppers <shelithlpeppers@gmail.com>
2019-02-20 21:19:38 -0600

    deleted failed hashing attempt and updated main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from hashutils import make_pw_hash, check_pw_hash
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -28,12 +28,12 @@ class Blog(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
-    pw_hash = db.Column(db.String(120))
+    password = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, username, password):
         self.username = username
-        self.pw_hash = make_pw_hash(password)
+        self.password = password
 
 @app.before_request
 def require_login():
@@ -94,7 +94,7 @@ def login():
         password = request.form['password']
 
         user = User.query.filter_by(username=username).first()      #if user doesn't exist, user == None
-        if user and check_pw_hash(password, user.pw_hash):                      #conditional breaks if user == None
+        if user and user.password == password:                      #conditional breaks if user == None
             session['username'] = username
             flash('Logged in')
             return redirect('/newpost')
